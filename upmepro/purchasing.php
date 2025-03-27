9b2994226d5d2920bf28a86d1cb9fcd0c1d725e0
Kim Tangpus <kimtangpus@gmail.com>
2025-03-27 16:19:39 +0800

    asdasd

--- upmepro/purchasing.php
+++ upmepro/purchasing.php
@@ -3,32 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard UI</title>
+    <title>Smart Purchasing</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel = "stylesheet" type = "text/css" href = "css/style.css" />  
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css" rel="stylesheet">
+
 </head>
 <body>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-2 sidebar">
-            <div class="py-3 px-4">
-                <h4>UpmePro</h4>
-            </div>
+        <div class="py-3 px-4 d-flex justify-content-center">
+        <img src="https://i.ibb.co/Txzk8Rg0/UPme-PRO-Revised-removebg-preview.png" alt="logo" class="img-fluid" style="width: 150px;">
+</div>
+
+          
             <div>
-                <a href="index.php" id="dashboard-link">Executive Dashboard</a>
-                <a href="order.php" id="order-entry-link">Order Entry & Billing</a>
-                <a href="service.php" id="service-management-link">Service Management</a>
-                <a href="receive.php" id="accounts-receivable-link">Accounts Receivable</a>
-                <a href="inventory.php" id="inventory-control-link">Inventory Control</a>
-                <a href="purchasing.php" id="smart-purchasing-link">Smart Purchasing</a>
-                <a href="payable.php" id="accounts-payable-link">Accounts Payable</a>
+                <a href="index.php" id="dashboard-link"><span class="mdi mdi-poll"></span>  Executive Dashboard</a>
+                <a href="order.php" id="order-entry-link"><span class="mdi mdi-note-text-outline"></span>  Order Entry & Billing</a>
+                <a href="service.php" id="service-management-link"><span class="mdi mdi-wrench-clock-outline"></span>  Service Management</a>
+                <a href="receive.php" id="accounts-receivable-link"><span class="mdi mdi-briefcase-clock-outline"></span>  Accounts Receivable</a>
+                <a href="inventory.php" id="inventory-control-link"><span class="mdi mdi-package-variant-closed"></span>  Inventory Control</a>
+                <a href="purchasing.php" id="smart-purchasing-link"><span class="mdi mdi-truck-outline"></span>  Smart Purchasing</a>
+                <a href="payable.php" id="accounts-payable-link"><span class="mdi mdi-briefcase-account-outline"></span>  Accounts Payable</a>
             </div>
 
+           
             <div class="sidebar-bottom">
-                <a href="maintenance.php" id="maintenances-link">Maintenances</a>
-                <a href="explorer.php" id="report-explorer-link">Report Explorer</a>
+                <a href="maintenance.php" id="maintenances-link"><span class="mdi mdi-database-outline"></span>  Maintenances</a>
+                <a href="explorer.php" id="report-explorer-link"><span class="mdi mdi-printer-pos-outline"></span>  Report Explorer</a>
             </div>
         </div>
 
