74724212695855d7ba7b2789c438581ff62e7da9
Kim Tangpus <kimtangpus@gmail.com>
2025-05-29 11:22:44 +0800

    before db

--- poslaravel/poslaravel/resources/js/components/ProductCard.vue
+++ poslaravel/poslaravel/resources/js/components/ProductCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-2 border rounded hover:shadow cursor-pointer text-center">
+  <div class="p-2 border rounded hover:shadow-lg  cursor-pointer text-center">
     <img :src="product.image" alt="" class="h-20 mx-auto mb-2 object-contain" />
     <div class="text-sm font-semibold">{{ product.name }}</div>
     <div class="text-sm text-gray-500">₱ {{ product.price.toFixed(2) }}</div>
